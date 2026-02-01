c2bc6299ed96b2cb3683d80f20291fea4cc9bf41
s-sap <ssap@gmx.com>
2026-01-31 22:31:41 -0600

    more variables

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
 fn main() {
-    let x :i32 = 5; // x is immutable
-    let x = x + 1; //  new x is now shadowing the old x variable
-    println!("x is now: {}", x); // 6
-    {
-	let x = x * 2;  // inner scope x 
-	println!("Inner Scope x is now: {}", x); // 12
+    let an_int = 5i32;
 
-    }
-
-    println!("the value of outer scope x: {}", x); // 6
+    println!("An integer: {}", an_int);
 }
